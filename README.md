52a7e5b14e276b412efcf0cf73cabeca49210231
mewmew <rnd0x00@gmail.com>
2015-03-23 20:13:52 +0100

    restructure: Initial commit.

---
+++ README.md
@@ -0,0 +1,55 @@
+## WIP
+
+This project is a *work in progress*. The implementation is *incomplete* and subject to change. The documentation can be inaccurate.
+
+# restructure
+
+[![GoDoc](https://godoc.org/decomp.org/x/cmd/restructure?status.svg)](https://godoc.org/decomp.org/x/cmd/restructure)
+
+`restructure` is a tool which recovers high-level control flow primitives from control flow graphs (e.g. *.dot -> *.json). It takes an unstructured CFG (in Graphviz DOT file format) as input and produces a structured CFG (in JSON), describes how the high-level control flow primitives relate to the nodes of the CFG.
+
+## Installation
+
+```shell
+go get decomp.org/x/cmd/restructure
+```
+
+## Usage
+
+```
+restructure [OPTION]... CFG.dot
+
+Flags:
+  -prims="": Comma-separated list of file names to control flow primitive descriptions (*.dot)
+  -q=false: Suppress non-error messages.
+```
+
+## Examples
+
+1) Recover the high-level control flow primitives from the control flow graph [foo.dot](testdata.dot).
+
+```bash
+$ restructure foo.dot
+// Output:
+// Isomorphism of "list" found at node "2":
+//    "A"="2"
+//    "B"="3"
+// Isomorphism of "if" found at node "1":
+//    "A"="1"
+//    "B"="list0"
+//    "C"="4"
+```
+
+INPUT:
+* [foo.dot](testdata/foo.dot): unstructured control flow graph.
+
+![foo.dot subgraph](https://raw.githubusercontent.com/decomp/restructure/master/testdata/foo.png)
+
+OUTPUT:
+* [foo.json]: structured control flow graph.
+
+## Public domain
+
+The source code and any original content of this repository is hereby released into the [public domain].
+
+[public domain]: https://creativecommons.org/publicdomain/zero/1.0/
---
+++ restructure.go
@@ -0,0 +1,92 @@
+// restructure is a tool which recovers high-level control flow primitives from
+// control flow graphs (e.g. *.dot -> *.json). It takes an unstructured CFG (in
+// Graphviz DOT file format) as input and produces a structured CFG (in JSON),
+// describes how the high-level control flow primitives relate to the nodes of
+// the CFG.
+//
+// Example input
+//
+//    digraph foo {
+//       1 -> 2
+//       1 -> 4
+//       2 -> 3
+//       3 -> 4
+//       1 [label="entry"]
+//       2
+//       3
+//       4 [label="exit"]
+//    }
+//
+// Example output
+//
+//    {
+//       "entry": "if0",
+//       "primitives": {
+//          "if0": {
+//             "primitive": "if",
+//             "nodes": {
+//                "A": "1",
+//                "B": "list0",
+//                "C": "4",
+//             },
+//          },
+//          "list0": {
+//             "primitive": "list",
+//             "nodes": {
+//                "A": "3",
+//                "B": "4",
+//             },
+//          },
+//       },
+//    }
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	// flagPrimitives is a comma-separated list of file names to control flow
+	// primitive descriptions, which are stored as Graphviz DOT files.
+	flagPrimitives string
+	// When flagQuiet is true, suppress non-error messages.
+	flagQuiet bool
+)
+
+func init() {
+	flag.StringVar(&flagPrimitives, "prims", "", "Comma-separated list of file names to control flow primitive descriptions (*.dot).")
+	flag.BoolVar(&flagQuiet, "q", false, "Suppress non-error messages.")
+	flag.Usage = usage
+}
+
+const use = `
+restructure [OPTION]... CFG.dot
+Recover control flow structures from control flow graphs (e.g. *.dot -> *.json).
+`
+
+func usage() {
+	fmt.Fprintln(os.Stderr, use[1:])
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	dotPath := flag.Arg(0)
+	err := restructure(dotPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// restructure attempts to recover the control flow structure of a given control
+// flow graph.
+func restructure(dotPath string) error {
+	return nil
+}
